a5905d1dba340649d9238d4c825751befcadf76d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-22 16:12:10 -0400

    fix: avoid crashing on non-existent path
    Other changes:
    - simpler error message when no path is given
    - use nohup on background process to keep it running even if our terminal is closed

--- src/rimraf.sh
+++ src/rimraf.sh
@@ -2,11 +2,17 @@
 set -e
 
 to_remove=$1
+
 if [ "$to_remove" == '' ];
 then
-  echo 'Error, you must specify a directory name to remove'
+  echo 'rimraf expects a pathname'
   exit 1
 fi
+
 destination=~/`basename $to_remove`-$RANDOM-$RANDOM-$RANDOM
-mv $to_remove $destination
-(rm -rf $destination) &
+
+if [ -e "$to_remove" ] || [ -h "$to_remove" ];
+then
+  mv "$to_remove" "$destination"
+  nohup rm -rf "$destination" &>/dev/null &
+fi
